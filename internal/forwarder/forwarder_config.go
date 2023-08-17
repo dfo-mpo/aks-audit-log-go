588ca4528af3a9a177e81f49266f460c18752dfb
Caroline132 <caroline.tippins@dfo-mpo.gc.ca>
2023-08-17 11:15:08 -0400

    make verboseLevel, postMaxRetries and postRetryIncrementalDelay configurable

--- internal/forwarder/forwarder_config.go
+++ internal/forwarder/forwarder_config.go
@@ -23,43 +23,62 @@ type ForwarderConfiguration struct {
 func (f *ForwarderConfiguration) InitConfig() *ForwarderConfiguration {
 	fmt.Println("InitConfig")
 
-	envVars := []struct {
-		value    *string
-		name   string
-		errorMsg string
-	}{
-		{&f.EhubNamespaceConnectionString, "EHUBNAMESPACECONNECTIONSTRING", "EhubNamespaceConnectionString is not set"},
-		{&f.EventHubName, "EVENTHUBNAME", "EventHubName is not set"},
-		{&f.BlobStorageConnectionString, "BLOBSTORAGECONNECTIONSTRING", "BlobStorageConnectionString is not set"},
-		{&f.BlobContainerName, "BLOBCONTAINERNAME", "BlobContainerName is not set"},
-		{&f.WebSinkURL, "WEBSINKURL", "WebSinkURL is not set"},
+	f.EhubNamespaceConnectionString = os.Getenv("EHUBNAMESPACECONNECTIONSTRING")
+	if f.EhubNamespaceConnectionString == "" {
+		log.Fatal("EhubNamespaceConnectionString is not set")
 	}
 
-	for _, envVar := range envVars {
-		*envVar.value = os.Getenv(envVar.name)
-		if *envVar.value == "" {
-			log.Fatal(envVar.errorMsg)
-		}
+	f.EventHubName = os.Getenv("EVENTHUBNAME")
+	if f.EventHubName == "" {
+		log.Fatal("EventHubName is not set")
 	}
 
-	verboseLevelFromENV := os.Getenv("VERBOSELEVEL")
-	if verboseLevelFromENV != "" {
-		verboseLevel, err := strconv.Atoi(verboseLevelFromENV)
-		if err != nil {
-			log.Fatal("VerboseLevel is not set")
-		}
-
-		f.VerboseLevel = verboseLevel
+	f.BlobStorageConnectionString = os.Getenv("BLOBSTORAGECONNECTIONSTRING")
+	if f.BlobStorageConnectionString == "" {
+		log.Fatal("BlobStorageConnectionString is not set")
 	}
 
-	f.PostMaxRetries = 5
-	f.PostRetryIncrementalDelay = 1000
+	f.BlobContainerName = os.Getenv("BLOBCONTAINERNAME")
+	if f.BlobContainerName == "" {
+		log.Fatal("BlobContainerName is not set")
+	}
+
+	f.WebSinkURL = os.Getenv("WEBSINKURL")
+	if f.WebSinkURL == "" {
+		log.Fatal("WebSinkURL is not set")
+	}
+
+	verboseLevelENV := os.Getenv("VERBOSELEVEL")
+  verboseLevel, err := strconv.Atoi(verboseLevelENV)
+  if verboseLevelENV == "" || err != nil {
+    f.VerboseLevel = 1
+  } else {
+    f.VerboseLevel = verboseLevel
+  }
+
+	postMaxRetriesENV := os.Getenv("POSTMAXRETRIES")
+  postMaxRetries, err := strconv.Atoi(postMaxRetriesENV)
+  if postMaxRetriesENV == "" || err != nil {
+    f.PostMaxRetries = 5
+  } else {
+    f.PostMaxRetries = postMaxRetries
+  }
+
+	postRetryIncrementalDelayENV := os.Getenv("POSTRETRYINCREMENTALDELAY")
+  postRetryIncrementalDelay, err := strconv.Atoi(postRetryIncrementalDelayENV)
+  if postRetryIncrementalDelayENV == "" || err != nil {
+    f.PostRetryIncrementalDelay = 1000
+  } else {
+    f.PostRetryIncrementalDelay = postRetryIncrementalDelay
+  }
 
 	if f.VerboseLevel > 3 {
 		fmt.Println("EventHubName: {0}", f.EventHubName)
 		fmt.Println("BlobContainerName: {0}", f.BlobContainerName)
 		fmt.Println("WebSinkURL : {0}", f.WebSinkURL)
 		fmt.Println("VerboseLevel: {0}", f.VerboseLevel)
+    fmt.Println("PostMaxRetries: {0}", f.PostMaxRetries)
+    fmt.Println("PostRetryIncrementalDelay: {0}", f.PostRetryIncrementalDelay)
 
 		fmt.Println("EhubNamespaceConnectionString length: {0}", len(f.EhubNamespaceConnectionString))
 		fmt.Println("BlobStorageConnectionString length: {0}", len(f.BlobStorageConnectionString))
