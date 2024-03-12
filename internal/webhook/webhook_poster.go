f3bc157a128c397390c1aabe0ee5f1d84b3d03c2
ctdfo <caroline.tippins@dfo-mpo.gc.ca>
2024-03-12 13:37:26 -0400

    keep this error log since it indicates max retries

--- internal/webhook/webhook_poster.go
+++ internal/webhook/webhook_poster.go
@@ -74,6 +74,7 @@ func (w *WebhookPoster) SendPost(auditEventStr string, mainEventName string, eve
 		return nil
 	} else {
 		forwarder.IncreaseErrors()
+		log.Error().Msgf("%s %d > **Error post response after max retries, gave up: [%d]", mainEventName, eventNumber, response.StatusCode)
 
 		return err
 	}
